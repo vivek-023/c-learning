0c41ea0f9326348e54ab046affc37efd45f670b8
vivek-023 <ranjanvivek054@gmail.com>
2022-09-24 20:32:17 +0530

    This is my first git commit

---
+++ sum.c
@@ -0,0 +1,15 @@
+ #include <stdio.h>
+ int main(){
+	 
+ // this line will print hello world	 
+ printf("hello world\n");
+ 
+ /** integer type variables
+     this program will add 2 numbers
+      output is 50+60=110  **/
+ int a=50;
+ int b=60;
+ printf("%d + %d = %d",b,a,a+b);
+
+	 
+ }
