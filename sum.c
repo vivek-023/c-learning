72573e551df2624c93ed33917a990627ac050584
vivek-023 <ranjanvivek054@gmail.com>
2022-09-24 21:05:20 +0530

    removed unneccesary lines of code

--- sum.c
+++ sum.c
@@ -1,19 +1,10 @@
  #include <stdio.h>
  int main(){
-	 
- // this line will print hello world	 
- printf("hello world\n");
- 
- /** integer type variables
-     this program will add 2 numbers
-      output is 50+60=110  **/
- int a;
- int b;
- printf("a : ");
- scanf("%d",&a);
- printf("b : ");
- scanf("%d",&b);
- printf("%d + %d = %d",b,a,a+b);
-
-	 
- }
+	 int a;
+	 int b;
+	 printf("enter first number : ");
+	 scanf("%d",&a);
+	 printf("enter second number : ");	 
+	 scanf("%d",&b);
+	 printf("%d + %d = %d",b,a,a+b);
+}
