f4f9043ef87b464158e8e5071ed755b71b2cb253
vivek-023 <ranjanvivek054@gmail.com>
2022-10-03 11:42:25 +0530

    New code

---
+++ for-loop/power.c
@@ -0,0 +1,12 @@
+ #include<stdio.h>
+ int main(){
+	int a,b,i,p=1;
+	printf("a: ");
+	scanf("%d",&a);
+	printf("b: ");
+	scanf("%d",&b);
+ for(i=1;i<=b;i=i+1){
+	 p=p*a;//sara logic ka khela isi line me h bhrata shree samjha to maja aya aur ap bta do kaisa h code
+ } printf("%d^%d=%d",a,b,p);
+	 
+}
