88e503c5fe1330bca46cd4d086604db9f7fb300e
vivek-023 <ranjanvivek054@gmail.com>
2022-10-06 22:20:39 +0530

    New code

--- if-else/identify_upper_case_lower_case.c
+++ if-else/identify_upper_case_lower_case.c
@@ -3,12 +3,14 @@
 int main(){
 	  /**it is capital letter
 	  it is small letter (COMPLETED)**/
-	  char X='b';
-	  if(X>='A' && X<='Z'){
-		  printf("%c is capital letter",X);
-	  } else if(X>='a' && X<='z'){
-			  printf("%c is small letter",X);
+	  char a;
+	  printf("a: ");
+	  scanf("%c",&a);
+	  if(a>='A' && a<='Z'){
+		  printf("%c is capital letter",a);
+	  } else if(a>='a' && a<='z'){
+			  printf("%c is small letter",a);
 		  }else {
-			  printf("%c is not an alphabet",X);
+			  printf("%c is not an alphabet",a);
 		  }
   }
