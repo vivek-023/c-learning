6242f70dd2f1c705114200655d4c3dc337ea2142
vivek-023 <ranjanvivek054@gmail.com>
2022-10-12 12:02:42 +0530

    New code

--- if-else/odd_even.c
+++ if-else/odd_even.c
@@ -2,7 +2,9 @@
 
 int main(){
 	  // numer is even otherwise it is odd (COMPLETED)
-	  int a=431; 
+	  int a; 
+	  printf("Enter a: ");
+	  scanf("%d",&a);
 	  if(a%2==0){
 		  printf("%d is even",a);
 	  } else if(a%2!=0){
